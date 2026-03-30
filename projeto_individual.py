fbb418b39f3e9fec0b5779342e7ecf464159c48d
Luís <a113231@uminho.pt>
2026-03-30 16:29:32 +0100

    feat(testes): adicionar 5 cenários de teste e validar resultados

--- projeto_individual.py
+++ projeto_individual.py
@@ -4,9 +4,9 @@ Created on Sat Mar 21 18:35:05 2026
 
 @author: Filipe
 """
-# bloco inicial dado
-blocos = [2,6,8,5]
 
+
+#Funções
 def avancar_direita(blocos, inicio):
     posicao = inicio # copia o valor de inicio para posicao
     # só avança se existir um bloco à frente e se esse bloco for acessível
@@ -22,8 +22,6 @@ def avancar_esquerda(blocos, inicio):
         posicao = posicao - 1
     return posicao
 
-print(avancar_direita(blocos, 0))
-print(avancar_esquerda(blocos, 3))
 
 def solucao(blocos):
     maior_distancia = 0  # guarda a maior distância encontrada, começa em 0 porque ainda não testámos nada
@@ -38,7 +36,27 @@ def solucao(blocos):
     
     return maior_distancia
 
+
+#Testes
+
+# bloco inicial dado
+blocos = [2,6,8,5] 
 print(solucao(blocos))
 
-blocos = [1, 5, 5, 2, 6]
-print(solucao(blocos))
+# bloco de 5 testes criado (2 do enunciado, 3 aleatórios)
+testes = [
+        ([2,6,8,5], 2),
+        ([1,5,5,2,6], 3),
+        ([1,1,1,1], 3),
+        ([5,4,3,2,1], 4),
+        ([1,2,3,4,5], 4),
+        ]
+
+# Executa cada teste e compara com o valor esperado
+for blocos, valor_esperado in testes:
+
+    distância = solucao(blocos) # distância máxima encontrada
+    
+    print("Blocos:", blocos)
+    print("Distância:", distância)
+    print("Valor esperado:", valor_esperado)
